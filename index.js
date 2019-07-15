b893803f22c442c8e1cca202f5d3ad1290a40a90
VorwerkD <48744770+VorwerkD@users.noreply.github.com>
2019-07-15 18:21:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ var recruitChans=['596613040762388480','596613065907109888','596613090900967440'
 
 
 client.on('message', msg => {
-	
+if(msg.content.startsWith(prefix){
 input=msg.content.toLowerCase()//to make all inputs - either command or parameters all uniform - if a command needs uppercase parameters etc, put above this line
 var inputRes =input.split(' ');
 var inputResCom= inputRes[0].substring(1);
@@ -84,6 +84,7 @@ b = FuzzySet(commands);
 const inCom =b.get(inputResCom);
 const inCom2 = inCom[0];
 input=inCom2[1];
+}
 if (input.startsWith(prefix+"test")){
 var res = input.substring(6); 
 console.log(res);
