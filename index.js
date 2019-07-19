e125033c7528aabfc8e6cc9ef2ff192959be1bff
VorwerkD <48744770+VorwerkD@users.noreply.github.com>
2019-07-19 12:27:22 +0100

    merge support and invite into help

--- index.js
+++ index.js
@@ -296,6 +296,8 @@ msg.channel.send("This word could not be found from UrbanDictionary.com");
   embed.addField(commands[x],
     commandHelp[x])
 }
+.addField("Join Vorwerk's Server at: https://discord.gg/GUKXv7j")
+.addField("Invite this bot to other servers with https://discordapp.com/oauth2/authorize?&client_id=523260461932740620&scope=bot&permissions=93250!")
   msg.channel.send({embed});
 }
 
