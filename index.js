769266f25d6ff46ad2e6c6d3e1c6b1a4ac2598f5
VorwerkD <48744770+VorwerkD@users.noreply.github.com>
2019-07-19 12:20:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ var isUpdating;
 var isUpdated;
 client.wait = require('util').promisify(setTimeout);
 var CronJob = require('cron').CronJob;
-var commands = ["help","support","ping","flip","lotto","kick","clearChat","live","urban","guildnum","console","gg","reset"];
+var commands = ["help","support","ping","flip","lotto","kick","clearChat","live","urban","guildnum","console","gg","reset","update"];
 var commandHelp = ["You're reading it!","Join our support server!",
 "Pongs!","Flips a coin","Gives you your lotto numbers- Doesn't gaurentte you win though :wink:",
 "kicks a valid user, just tag them `.kick @notVorwerk#6126` for example\nRequires Admin role",
 "Clears the current channel of messages <14 days old add a number<100 to clear - defaults to 100 `.clearChat 50` for example \nRequires Admin role",
 "check to see if the given twitch streamer is live `.live Vorwerk_D` for example","Searches urban dictionary for a word/phrase. Attach -r for a random word",
-"Updates the guild numbers channels","Logs the input to the console ","Gives the swgoh.gg link of the requested user `.gg Vorwerk17` for example","shuts down and resets the bots login. Vorwerk only"]
+"Updates the guild numbers channels","Logs the input to the console ","Gives the swgoh.gg link of the requested user `.gg Vorwerk17` for example","shuts down and resets the bots login. Vorwerk only","Manually updates the cached guilds"]
 var olafTest = '570738555773648897'//chan id
 //twitch stuffs
 var streamList = ["mL7support","yautjaridley"]
