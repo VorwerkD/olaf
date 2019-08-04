a2cac4a7e7f3ebd41bda8fcc53da4b3868975c04
VorwerkD <48744770+VorwerkD@users.noreply.github.com>
2019-08-03 22:03:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -114,8 +114,17 @@ for(var x = 0; x<names.length;x++){
   console.log(names[x]);
   discordNot.push(names[x]);
 }}
-msg.channel.send("INGAME NAMES NOT IN DISCORD\n"+names)
-msg.channel.send("Discord names not in game\n"+allianceUsers)
+var names1 = names.slice(0,names.length/2);
+var names2= names.slice(names.length/2,names.length);
+var allianceUsers1 = allianceUsers.slice(0,allianceUsers.length/2);
+var allianceUsers2 = allianceUsers.slice(allianceUsers.length/2,allianceUsers.length);
+
+msg.channel.send("INGAME NAMES NOT IN DISCORD")
+msg.channel.send(names1);
+msg.channel.send(names2);
+msg.channel.send("Discord names not in game");
+msg.channel.send(allianceUsers1);
+msg.channel.send(allianceUsers2);
 
 })();
   }
