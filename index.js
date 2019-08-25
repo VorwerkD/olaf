de521aa3b938cbe34aa1b81ae9e621b3f240a175
VorwerkD <48744770+VorwerkD@users.noreply.github.com>
2019-08-25 00:09:17 -0400

    rename embed title

--- index.js
+++ index.js
@@ -606,7 +606,7 @@ if(milliseconds<listEndTime&&milliseconds>listStartTime){
 //console.log(liveEvents);
 
 const embed = new Discord.RichEmbed()
-  .setTitle("Live Events:")
+  .setTitle("SWGOH Today:")
   .setAuthor(client.user.username,client.user.avatarURL)
   .setFooter("Made by Vorwerk")
   .setTimestamp()
