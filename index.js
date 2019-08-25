f0826492993c3d7bc693ebc2256f8f8699f3b701
VorwerkD <48744770+VorwerkD@users.noreply.github.com>
2019-08-25 09:18:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -582,7 +582,7 @@ var dateString = (month+1)+ "/" +date + "/" + year;
 liveEvents.push(wakeups[day]);
 liveEventsDes.push("Lets get those tickets in! 1 LS/DS/Cantina energy = 1 ticket !\nThe more tickets we earn means the more often we raid!")
 liveEvents.push("Today is: "+dayConvert(day)+" the "+date+"th of "+monthConvert(month));
-liveEventsDes.push("*The Guild Activity today is: "+activities[day]+"*");
+liveEventsDes.push("**The Guild Activity today is: **"+activities[day]);
 for(var x = 0; x<events.length;x++){
 var eventName = events[x].nameKey
 var eventDes = events[x].summaryKey
