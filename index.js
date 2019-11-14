53f706d13e81792fe616c90b33df93c755d15a01
VorwerkD <48744770+VorwerkD@users.noreply.github.com>
2019-11-14 11:09:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ client.on('message', msg => {
   //FLAG
   var regex = /\[([\d\w]+)\]/g;
   if(input.startsWith(prefix+"test")){
- msg.channel.send(":MiraSleep:");
+ msg.channel.send("<:MiraSleep:644569524326236162>");
   }
   if(input.startsWith(prefix+"addtochannel")){
     if(msg.member.roles.some(r=>["Royal Guards"].includes(r.name))){
